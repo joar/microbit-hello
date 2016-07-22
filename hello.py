f1e8e9a69de66f2d5562a5a15ea0a142a8af4dd4
Joar Wandborg <joar@wandborg.se>
2016-07-22 14:43:59 +0200

    Fix sine stuff

--- hello.py
+++ hello.py
@@ -1,8 +1,23 @@
+import math
+from collections import namedtuple
+
 import microbit
+
+now = microbit.running_time
+
+
+def hypot(x, y):
+    return math.sqrt(math.pow(x, 2) + math.pow(y, 2))
 
 
 # TODO: Use namedtuples
 class State:
+    __slots__ = (
+        'programs',
+        'active',
+        '_active_index',
+    )
+
     def __init__(self, programs=None):
         self.programs = programs or []
         self.active = None
@@ -25,13 +40,19 @@ class State:
 
 
 class Handlers:
+    __slots__ = (
+        'button_a',
+        'button_b',
+        'interrupt',
+    )
+
     def __init__(self, button_a=None, button_b=None, interrupt=None):
         self.button_a = button_a
         self.button_b = button_b
         self.interrupt = interrupt
 
 
-DEFAULT_HANDLERS = Handlers(button_a=lambda: STATE.start_next())
+DEFAULT_HANDLERS = Handlers(button_a=lambda *a: STATE.start_next())
 
 STATE = State()
 HANDLERS = DEFAULT_HANDLERS
@@ -52,23 +73,7 @@ class ButtonB(Input):
 def program(func):
     """Registering decorator for programs"""
     STATE.programs.append(func)
-
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-
-    for i in ['name', 'qualname', 'annotations', 'doc', 'module']:
-        name = '__{}__'.format(i)
-        try:
-            setattr(wrapper, name, getattr(func, name))
-        except AttributeError:
-            pass
-
-    try:
-        wrapper.__dict__.update(func.__dict__)
-    except AttributeError:
-        pass
-
-    return wrapper
+    return func
 
 
 def check_input():
@@ -92,12 +97,51 @@ def sleep(millis):
 def render_clocks(self):
     microbit.display.show(microbit.Image.ALL_CLOCKS, loop=True, delay=100)
 
+brightness_max = 9
+half_brightness_max = brightness_max / 2
+two_pi = math.pi * 2
+
+origo_x, origo_y = 2, 2
+
+max_distance = hypot(origo_x, origo_y)
+double_max_distance = max_distance * 2
+
+
+@program
+def draw_gradient0():
+    offset = 0
+    last_t = now()
+
+    while True:
+        for x in range(0, 5):
+            dist_x = x - origo_x
+            for y in range(0, 5):
+                dist_y = y - origo_y
+
+                distance = (math.fabs(hypot(dist_x, dist_y)) /
+                            double_max_distance)
+                distance = (distance + offset) % 1
+
+                sin = math.sin(distance * two_pi - math.pi)
+
+                value = round(sin * half_brightness_max + half_brightness_max)
+
+                microbit.display.set_pixel(x, y, value)
+
+        t_now = now()
+        delta_t = t_now - last_t
+        offset += delta_t * 0.001
+        last_t = t_now
+        yield from sleep(10)
+
 
 @program
 def draw_gradient1():
     offset = 0
     max_value = 9
     max_index = 5
+
+    last_t = now()
 
     while True:
         for x in range(0, 5):
@@ -108,9 +152,11 @@ def draw_gradient1():
                 value = round(relative_pos * max_value)
                 microbit.display.set_pixel(x, y, value)
 
-        yield from sleep(100)
-        offset += 1
-        offset %= max_index
+        yield from sleep(1)
+        t_now = now()
+        delta_t = t_now - last_t
+        offset += delta_t * 0.01
+        last_t = t_now
 
 
 @program
@@ -119,25 +165,23 @@ def draw_gradient2():
     max_value = 9
     max_index = 5
 
+    last_t = now()
+
     while True:
         for x in range(0, 5):
             for y in range(0, 5):
-                pos = x * y
+                pos = x + y
                 offset_pos = (offset + pos) % max_index
                 relative_pos = offset_pos / max_index
                 value = round(relative_pos * max_value)
                 microbit.display.set_pixel(x, y, value)
 
-        yield from sleep(100)
-        offset += 1
-        offset %= max_index
+        yield from sleep(1)
 
-
-@program
-def long_running():
-    while True:
-        microbit.display.scroll('Hello', wait=False)
-        yield from sleep(1000 * 1000)
+        t_now = now()
+        delta_t = t_now - last_t
+        offset += delta_t * 0.01
+        last_t = t_now
 
 
 print(STATE)
